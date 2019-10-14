fa707ba3803e845e82da9c0b3e3f9229b80e7d3e
Will Hathaway <william.hathaway38@gmail.com>
2019-10-13 21:08:52 -0400

    final changes made to styles, wins and losses now remain after a new game. new game stars automatically after a win/loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,55 +2,59 @@ $(document).ready(function () {
 
     //define variables:
 
-    let targetNumber;
-    let diamond;
-    let emerald;
-    let lapis;
-    let redstone;
+    let targetNumber = Math.floor(Math.random() * 120) + 19;
+    let diamond = Math.floor(Math.random() * 12) + 1;
+    let emerald = Math.floor(Math.random() * 12) + 1;
+    let lapis = Math.floor(Math.random() * 12) + 1;
+    let redstone = Math.floor(Math.random() * 12) + 1;
     let wins = 0;
     let losses = 0;
     let score = 0;
 
-    $('#newGameBtn').on('click', function newGame() {
+    let values = [targetNumber, diamond, emerald, lapis, redstone]
 
+    for (let i = 0; i < values.length; i++){
+        console.log(values[i]);
+    }
+
+    $('#randomNumber').text(targetNumber);
+
+    function newGame() {
         targetNumber = Math.floor(Math.random() * 120) + 19;
         diamond = Math.floor(Math.random() * 12) + 1;
         emerald = Math.floor(Math.random() * 12) + 1;
         lapis = Math.floor(Math.random() * 12) + 1;
         redstone = Math.floor(Math.random() * 12) + 1;
+        score = 0;
 
-
-
+        $("#currentScore").text(score)
         $('#randomNumber').text(targetNumber);
+        
+        for (let i = 0; i < values.length; i++){
+            console.log(values[i]);
+        }
+        
+    };
 
-        console.log('target number: ' + targetNumber);
-        console.log('diamond value: ' + diamond);
-        console.log('emerald value: ' + emerald);
-        console.log('lapis value: ' + lapis);
-        console.log('redstone value: ' + redstone);
+    $("#diamond").click(function () {
+        score = score + diamond;
+        $('#currentScore').text(score);
         console.log('score: ' + score);
-
-        $("#diamond").click(function () {
-            score = score + diamond;
-            $('#currentScore').text(score);
-            console.log('score: ' + score);
-        });
-        $("#emerald").click(function () {
-            score = score + emerald;
-            $('#currentScore').text(score);
-            console.log('score: ' + score);
-        });
-        $("#lapis").click(function () {
-            score = score + lapis;
-            $('#currentScore').text(score);
-            console.log('score: ' + score);
-        });
-        $("#redstone").click(function () {
-            score = score + redstone;
-            $('#currentScore').text(score);
-            console.log('score: ' + score);
-        });
-
+    });
+    $("#emerald").click(function () {
+        score = score + emerald;
+        $('#currentScore').text(score);
+        console.log('score: ' + score);
+    });
+    $("#lapis").click(function () {
+        score = score + lapis;
+        $('#currentScore').text(score);
+        console.log('score: ' + score);
+    });
+    $("#redstone").click(function () {
+        score = score + redstone;
+        $('#currentScore').text(score);
+        console.log('score: ' + score);
     });
 
     $('.crystal').click(function () {
@@ -59,11 +63,12 @@ $(document).ready(function () {
             wins++;
             $('#wins').text(wins);
             $('#result').text('You win!')
+            newGame();
         } else if (score > targetNumber) {
             losses++;
             $('#losses').text(losses);
             $('#result').text('You lose');
+            newGame();
         }
     });
-
 });
